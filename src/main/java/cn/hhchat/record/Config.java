72f6be8d83f382e5b1c44fd8efe784ba3854afe2
hh23485 <hh23485@126.com>
2018-03-21 00:28:13 +0800

    增加了新的参数，白名单、黑名单、测试、是否获取多人本、是否获取评论

--- src/main/java/cn/hhchat/record/Config.java
+++ src/main/java/cn/hhchat/record/Config.java
@@ -1,5 +1,6 @@
 package cn.hhchat.record;
 
+import com.xiaoleilu.hutool.collection.CollectionUtil;
 import com.xiaoleilu.hutool.json.JSONObject;
 import com.xiaoleilu.hutool.json.JSONUtil;
 import lombok.Data;
@@ -28,15 +29,40 @@ public class Config {
     public static String ROOT = "./";
     public static String IMG_MAX_SIZE = "500";
     public static String EMAIL;
+    public static Boolean TEST;
     public static String PASSWORD;
     public static Boolean REVERSE = false;
     public static Boolean DOWNLOAD_IMAGES = false;
     public static Boolean USE_JSON_DATA = false;
+    public static Boolean WANT_COMMENTS = false;
+    public static Boolean WANT_MULTIDREAM = true;
     public static List<String> WHITE_LIST = new ArrayList<>();
+    public static List<String> BLACK_LIST = new ArrayList<>();
 
 
     public static String getHtml(String path) {
         return DOMAIN + path;
+    }
+
+    public static Boolean inWhiteList(String title) {
+        if(CollectionUtil.isEmpty(WHITE_LIST)){
+            return true;
+        }
+        for (String str : WHITE_LIST) {
+            if (title.startsWith(str)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Boolean inBlackList(String title) {
+        for (String str : BLACK_LIST) {
+            if (str.equals(title)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static Boolean load() {
@@ -54,10 +80,13 @@ public class Config {
                 PASSWORD = String.valueOf(config.get("PASSWORD"));
                 PASSWORD = String.valueOf(config.get("PASSWORD"));
                 REVERSE = Boolean.valueOf(String.valueOf(config.get("REVERSE")));
-                REVERSE = Boolean.valueOf(String.valueOf(config.get("REVERSE")));
+                TEST = Boolean.valueOf(String.valueOf(config.get("TEST")));
                 DOWNLOAD_IMAGES = Boolean.valueOf(String.valueOf(config.get("DOWNLOAD_IMAGES")));
                 USE_JSON_DATA = Boolean.valueOf(String.valueOf(config.get("USE_JSON_DATA")));
+                WANT_COMMENTS = Boolean.valueOf(String.valueOf(config.get("WANT_COMMENTS")));
+                WANT_MULTIDREAM = Boolean.valueOf(String.valueOf(config.get("WANT_MULTIDREAM")));
                 WHITE_LIST = config.getJSONArray("WHITE_LIST").toList(String.class);
+                BLACK_LIST = config.getJSONArray("BLACK_LIST").toList(String.class);
                 return true;
             }else{
                 log.error(" => 没有找到配置文件文件");
